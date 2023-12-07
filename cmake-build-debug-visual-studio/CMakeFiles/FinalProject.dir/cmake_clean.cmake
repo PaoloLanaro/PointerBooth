bc75aa9ee9f2cef4f8f0aa369de7d1ce587aba2d
David Busnel <dbusnel@icloud.com>
2023-12-06 20:11:26 -0500

    got libraries working and changed build toolchain to visual studio

---
+++ cmake-build-debug-visual-studio/CMakeFiles/FinalProject.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FinalProject.dir/driver.cpp.obj"
+  "CMakeFiles/FinalProject.dir/driver.cpp.obj.d"
+  "CMakeFiles/FinalProject.dir/lib/include/Filter.cpp.obj"
+  "CMakeFiles/FinalProject.dir/lib/include/Filter.cpp.obj.d"
+  "FinalProject.exe"
+  "FinalProject.exe.manifest"
+  "FinalProject.lib"
+  "FinalProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FinalProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug-visual-studio/CMakeFiles/FinalProject.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+﻿# Empty compiler generated dependencies file for FinalProject.
+# This may be replaced when dependencies are built.
